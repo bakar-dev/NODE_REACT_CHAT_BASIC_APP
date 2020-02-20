a4aa2ec11a5fa5a6be46c8df09d51cdcaf7d857b
Bakr Hamid <baker.pucit@gmail.com>
2020-02-20 17:39:16 +0500

    first commit - basic application structer of client+Server added

---
+++ Server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+
+const router = require("./router");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on("connection", socket => {
+  console.log("New User connected..!!!");
+  socket.on("disconnet", () => {
+    console.log("User disconnected...");
+  });
+});
+
+app.use(router);
+
+server.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`));
